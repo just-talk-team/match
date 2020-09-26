c862e5d04e17d9e5c3e923b5f080dfb2d0969d4a
Anthony Bravo <anthony.bravo.berrio@gmail.com>
2020-09-26 18:43:49 -0500

    Matches controller agregado

--- app/controllers/match-controller.js
+++ app/controllers/match-controller.js
@@ -6,7 +6,7 @@ function productsApi(app) {
   app.use("/matches", router);
 
   router.get("/", async (req, res, next) => {
-    res.send("matches test");
+    res.send("matches controller");
   });
 }
 
