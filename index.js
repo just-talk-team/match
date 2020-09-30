e1f84b2232fea452d422176c18ee25d10dd63339
Jimena Ruiz <u201514642@upc.edu.pe>
2020-09-30 13:14:52 -0500

    Se organiza codigo en vista index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 const port = process.env.PORT || 3000;
 
 const matchController = require("./app/controllers/match.controller");
